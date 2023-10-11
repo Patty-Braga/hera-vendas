f89d9990ad7f5ddc066260c3b0c1a90d6d36d358
MeDourado <mariaeduarda.s.dourado@gmail.com>
2023-10-11 19:46:35 -0300

    Fix: Validação Usuario - Nome

--- src/validacoes/usuario.js
+++ src/validacoes/usuario.js
@@ -1,22 +1,36 @@
 const joi = require("joi");
 
 const validacaoUsuario = joi.object({
-  nome: joi.string().required().messages({
-    "any.required": "O campo nome é obrigatório",
-    "string.empty": "O campo nome é obrigatório",
-    'string.base': 'O campo nome deve conter apenas caracteres válidos'
-  }),
+  nome: joi
+    .string()
+    .min(3)
+    .required()
+    .pattern(new RegExp("^[a-zA-ZÀ-ÿ\u00f1\u00d1]+$"))
+    .messages({
+      "any.required": "O campo nome é obrigatório",
+      "string.empty": "O campo nome é obrigatório",
+      "string.pattern.base":
+        "O campo nome deve conter apenas caracteres válidos",
+      "string.min": "O campo nome precisa ter, no mínimo, 3 caracteres",
+    }),
+
   email: joi.string().email().required().messages({
     "any.required": "O campo email é obrigatório",
     "string.empty": "O campo email é obrigatório",
     "string.email": "O campo email precisa ser um email válido",
   }),
-  senha: joi.string().min(5).required().pattern(new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\\$%^&*])")).messages({
-    "any.required": "O campo senha é obrigatório",
-    "string.empty": "O campo senha é obrigatório",
-    "string.min": "O campo senha precisa ter, no mínimo, 5 caracteres",
-    "string.pattern.base": "A senha deve conter pelo menos uma letra maiúscula, uma letra minúscula, um número e um caractere especial",
-  }),
+  senha: joi
+    .string()
+    .min(5)
+    .required()
+    .pattern(new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\\$%^&*])"))
+    .messages({
+      "any.required": "O campo senha é obrigatório",
+      "string.empty": "O campo senha é obrigatório",
+      "string.min": "O campo senha precisa ter, no mínimo, 5 caracteres",
+      "string.pattern.base":
+        "A senha deve conter pelo menos uma letra maiúscula, uma letra minúscula, um número e um caractere especial",
+    }),
 });
 
 module.exports = validacaoUsuario;
