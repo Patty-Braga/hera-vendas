083eccdd29c1e5661c200afd5c635a8a444d4439
MeDourado <mariaeduarda.s.dourado@gmail.com>
2023-10-11 19:29:41 -0300

    Fix: Mensagem erro Catch

--- src/controladores/usuarios.js
+++ src/controladores/usuarios.js
@@ -1,91 +1,90 @@
-const knex = require('../conexao')
-const bcrypt = require('bcrypt');
+const knex = require("../conexao");
+const bcrypt = require("bcrypt");
 
 const cadastrarUsuario = async (req, res) => {
-    const { nome, email, senha } = req.body
-    try {
-        const usuarioEncontrado = await knex('usuarios').where({ email }).first();
+  const { nome, email, senha } = req.body;
+  try {
+    const usuarioEncontrado = await knex("usuarios").where({ email }).first();
 
-        if (usuarioEncontrado) {
-            return res.status(400).json({ mensagem: "Este email já pertence a outro usuário" })
-        }
-
-        const senhaCriptografada = await bcrypt.hash(senha, 10);
-        console.log(nome, email, senha);
-
-        await knex('usuarios')
-            .insert({
-                nome,
-                email,
-                senha: senhaCriptografada
-            });
-
-        return res.status(201).json({ mensagem: "Usuário cadastrado com sucesso!" });
-
-    } catch (error) {
-        return res.status(500).json(error.message);
+    if (usuarioEncontrado) {
+      return res
+        .status(400)
+        .json({ mensagem: "Este email já pertence a outro usuário" });
     }
-}
+
+    const senhaCriptografada = await bcrypt.hash(senha, 10);
+
+    await knex("usuarios").insert({
+      nome,
+      email,
+      senha: senhaCriptografada,
+    });
+
+    return res
+      .status(201)
+      .json({ mensagem: "Usuário cadastrado com sucesso!" });
+  } catch (error) {
+    return res.status(500).json(error.message);
+  }
+};
 
 const detalharUsuario = async (req, res) => {
-    try {
-        const usuarioAutenticado = req.usuario;
+  try {
+    const usuarioAutenticado = req.usuario;
 
-        if (!usuarioAutenticado) {
-            return res.status(404).json({ mensagem: 'Usuário não encontrado' });
-        }
-
-        const { id, nome, email } = usuarioAutenticado;
-
-        return res.status(200).json({
-            id,
-            nome,
-            email,
-        });
-    } catch (error) {
-        return res.status(500).json({ mensagem: 'Ocorreu um erro durante a busca do usuário' });
+    if (!usuarioAutenticado) {
+      return res.status(404).json({ mensagem: "Usuário não encontrado" });
     }
-}
+
+    const { id, nome, email } = usuarioAutenticado;
+
+    return res.status(200).json({
+      id,
+      nome,
+      email,
+    });
+  } catch (error) {
+    return res.status(500).json({ mensagem: error.message });
+  }
+};
 
 const atualizarUsuario = async (req, res) => {
-    const { id } = req.usuario;
-    const { nome, email, senha } = req.body;
+  const { id } = req.usuario;
+  const { nome, email, senha } = req.body;
 
-    try {
-        const usuarioExiste = await knex('usuarios').where({ id }).first();
+  try {
+    const usuarioExiste = await knex("usuarios").where({ id }).first();
 
-        if (!usuarioExiste) {
-            return res.status(404).json({ mensagem: 'Usuario não encontrado' });
-        }
-
-        const senhaCriptografada = await bcrypt.hash(senha, 10);
-
-        if (email !== req.usuario.email) {
-            const emailUsuarioExiste = await knex('usuarios')
-                .where({ email })
-                .first();
-
-            if (emailUsuarioExiste) {
-                return res.status(400).json({ mensagem: 'O Email já existe.' });
-            }
-        }
-
-        await knex('usuarios')
-            .where({ id })
-            .update({
-                nome,
-                email,
-                senha: senhaCriptografada,
-            });
-
-        return res.status(201).send({ mensagem: 'Usuário editado com sucesso' });
-    } catch (error) {
-        return res.status(500).json(error.message);
+    if (!usuarioExiste) {
+      return res.status(404).json({ mensagem: "Usuario não encontrado" });
     }
-}
+
+    const senhaCriptografada = await bcrypt.hash(senha, 10);
+
+    if (email !== req.usuario.email) {
+      const emailUsuarioExiste = await knex("usuarios")
+        .where({ email })
+        .first();
+
+      if (emailUsuarioExiste) {
+        return res.status(400).json({ mensagem: "O Email já existe." });
+      }
+    }
+
+    await knex("usuarios").where({ id }).update({
+      nome,
+      email,
+      senha: senhaCriptografada,
+    });
+
+    return res.status(201).send({ mensagem: "Usuário editado com sucesso" });
+  } catch (error) {
+    return res.status(500).json(error.message);
+  }
+};
 
 module.exports = {
-    cadastrarUsuario,
-    detalharUsuario,
-    atualizarUsuario
-}
+  cadastrarUsuario,
+  detalharUsuario,
+  atualizarUsuario,
+};
