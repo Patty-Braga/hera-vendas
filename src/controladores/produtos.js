c16dec886e8f8bf5cf7eca7a8740d1a1634c0e19
MeDourado <mariaeduarda.s.dourado@gmail.com>
2023-10-18 12:14:17 -0300

    Feat: Detalhar Produto

--- src/controladores/produtos.js
+++ src/controladores/produtos.js
@@ -1,64 +1,78 @@
 const knex = require("../conexao");
 
 const cadastrarProduto = async (req, res) => {
-    const { descricao, quantidade_estoque, valor, categoria_id } = req.body;
+  const { descricao, quantidade_estoque, valor, categoria_id } = req.body;
 
-    try {
-        const quantidadeCategoria = await knex("categorias");
+  try {
+    const quantidadeCategoria = await knex("categorias");
 
-        if (categoria_id > quantidadeCategoria.length) {
-            return res.status(404).json({ mensagem: "A categoria informada não existe, informe uma categoria válida para prosseguir." });
-        }
-
-        await knex("produtos").insert({
-            descricao,
-            quantidade_estoque,
-            valor,
-            categoria_id
-        });
-
-        return res.status(201).json({ mensagem: "Produto cadastrado com sucesso!" });
-    } catch (error) {
-        return res.status(500).json(error.message);
+    if (categoria_id > quantidadeCategoria.length) {
+      return res.status(404).json({
+        mensagem:
+          "A categoria informada não existe, informe uma categoria válida para prosseguir.",
+      });
     }
+
+    await knex("produtos").insert({
+      descricao,
+      quantidade_estoque,
+      valor,
+      categoria_id,
+    });
+
+    return res
+      .status(201)
+      .json({ mensagem: "Produto cadastrado com sucesso!" });
+  } catch (error) {
+    return res.status(500).json(error.message);
+  }
 };
 
 const editarProduto = async (req, res) => {
-    try {
-
-    } catch (error) {
-        return res.status(500).json(error.message);
-    }
-}
+  try {
+  } catch (error) {
+    return res.status(500).json(error.message);
+  }
+};
 
 const listarProdutos = async (req, res) => {
-    try {
-
-    } catch (error) {
-        return res.status(500).json(error.message);
-    }
-}
+  try {
+  } catch (error) {
+    return res.status(500).json(error.message);
+  }
+};
 
 const detalharProduto = async (req, res) => {
-    try {
+  const { id } = req.params;
 
-    } catch (error) {
-        return res.status(500).json(error.message);
+  try {
+    const produtoEncontrado = await knex
+      .select("*")
+      .from("produtos")
+      .where({ id })
+      .first();
+
+    if (!produtoEncontrado) {
+      return res.status(404).json({ mensagem: "Produto não encontrado" });
     }
-}
+
+    return res.status(200).json(produtoEncontrado);
+  } catch (error) {
+    return res.status(500).json(error.message);
+  }
+};
 
 const excluirProduto = async (req, res) => {
-    try {
-
-    } catch (error) {
-        return res.status(500).json(error.message);
-    }
-}
+  try {
+  } catch (error) {
+    return res.status(500).json(error.message);
+  }
+};
 
 module.exports = {
-    cadastrarProduto,
-    editarProduto,
-    listarProdutos,
-    detalharProduto,
-    excluirProduto
-}
+  cadastrarProduto,
+  editarProduto,
+  listarProdutos,
+  detalharProduto,
+  excluirProduto,
+};
