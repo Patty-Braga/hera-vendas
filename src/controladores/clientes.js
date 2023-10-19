0840460d2cda1c93718ddb4cf1e9b0876aed5cd0
Thamiris Maria <thamirismariasouzadeoliveira@gmail.com>
2023-10-19 18:04:05 -0300

    feat: detalha cliente

--- src/controladores/clientes.js
+++ src/controladores/clientes.js
@@ -46,7 +46,15 @@ const listarClientes = async (req, res) => {
 
 const detalharCliente = async (req, res) => {
   try {
+    const { id } = req.params;
 
+    const clienteEncontrado = await knex.select("*").from("clientes").where({ id }).first();
+
+    if (!clienteEncontrado) {
+      return res.status(404).json({ mensagem: 'Cliente não encontrado' });
+    }
+
+    return res.status(200).json(clienteEncontrado);
   } catch (error) {
     return res.status(500).json({ mensagem: error.message });
   }
