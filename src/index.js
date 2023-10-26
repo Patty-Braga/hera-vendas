5900eef82f2c9cc2908914253e60c9f53751b2b1
Thamiris Maria <73439911+ThamirisMaria@users.noreply.github.com>
2023-10-26 18:04:25 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,6 +9,6 @@ app.use(express.json());
 app.use(cors());
 app.use(rotas);
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3003
 
 app.listen(port, () => console.log(`Servidor on na porta: ${port} 🚀`))
